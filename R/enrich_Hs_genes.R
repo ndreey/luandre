475d4ab37f0d60ade7ebf001766c16d67384e8aa
ndreey <andbou95@gmail.com>
2022-12-12 15:04:09 +0100

    First commit of enrich_Hs_genes()

---
+++ R/enrich_Hs_genes.R
@@ -0,0 +1,36 @@
+#' Enrich genes with KEGG or GO (Homo sapiens)
+#'
+#' Returns a enrichResult class using a vector of ENTREZID's and specifying
+#' which database to enrich from (KEGG or GO) using enrichGO or enrichKEGG
+#' function from clusterProfiler. If db = "GO", ontology must be chosen.
+#' Biological Processes ("BP"), Cellular Components ("CC") or Molecular
+#' Functions ("MF").
+#'
+#' @param entrez vector with ENTREZID to enrich
+#' @param db "KEGG" or "GO"
+#' @param ontology "BP", "CC" or "MF"
+#'
+#' @import clusterProfiler
+#' @return enrichResult class
+#' @export
+#'
+enrich_Hs_genes <- function(entrez, db, ontology = NULL) {
+
+  if (db == "GO") {
+    # Enrichment GO
+    rich_res <- clusterProfiler::enrichGO(gene          = entrez,
+                                          OrgDb         = org.Hs.eg.db,
+                                          ont           = ontology,
+                                          pAdjustMethod = "BH",
+                                          pvalueCutoff  = 0.05)
+
+  } else if (db == "KEGG") {
+    # Enrichment KEGG
+    rich_res <- clusterProfiler::enrichKEGG(gene          = entrez,
+                                            organism      = "hsa",
+                                            keyType       = "kegg",
+                                            pAdjustMethod = "BH",
+                                            pvalueCutoff  = 0.05)
+  }
+  return(rich_res)
+}
